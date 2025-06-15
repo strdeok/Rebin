d728c2a8587fba43b61ccbd412e74988da4d8cce
strdeok <ejraks1548@gmail.com>
2025-06-15 19:42:29 +0900

    remove: 기능 축소

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,12 @@ import Login from "./pages/beforeLogin/login/login";
 import Main from "./pages/afterLogin/main/main";
 import Mypage from "./pages/afterLogin/mypage/mypage";
 import MainLayout from "./pages/afterLogin/mainLayout";
-import ScanningQR from "./pages/afterLogin/main/scanningQR";
-import Authenticated from "./pages/afterLogin/main/components/authenticated";
 import AroundTrash from "./pages/afterLogin/aroundTrash/aronudTrash";
-import ChatPage from "./pages/afterLogin/mypage/chatBot/chatPage";
-import EditNickName from "./pages/afterLogin/mypage/edtiNickName/editNickName";
+import ChatPage from "./pages/afterLogin/chat-bot/chatPage";
 import Favorites from "./pages/afterLogin/mypage/favorites/favorites";
-import PaymentHistory from "./pages/afterLogin/mypage/paymentHistory/paymentHistory";
 import TrashHistory from "./pages/afterLogin/mypage/trashHistory/trashHistory";
-import ScrollTop from "./util/scrollTop";
-import Store from "./pages/afterLogin/store/store";
+import ScrollTop from "./utils/scrollTop";
+import SetUserInfo from "./pages/beforeLogin/login/setUserInfo";
 
 function App() {
   return (
@@ -22,23 +18,18 @@ function App() {
       <Routes>
         <Route path="/" element={<OnBoarding />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SetUserInfo />} />
 
         <Route element={<MainLayout />}>
           <Route path="/main" element={<Main />} />
           <Route path="/around-trash" element={<AroundTrash />} />
-          <Route path="/store" element={<Store />} />
           <Route path="/mypage" element={<Mypage />} />
+          <Route path="/chat-bot" element={<ChatPage />} />
 
           {/* 마이페이지 하위 페이지 */}
-          <Route path="/mypage/chat-bot" element={<ChatPage />} />
-          <Route path="/mypage/edit-nickname" element={<EditNickName />} />
           <Route path="/mypage/favorites" element={<Favorites />} />
-          <Route path="/mypage/payment-history" element={<PaymentHistory />} />
           <Route path="/mypage/trash-history" element={<TrashHistory />} />
         </Route>
-
-        <Route path="scanning-qr" element={<ScanningQR />} />
-        <Route path="auth-completed" element={<Authenticated />} />
       </Routes>
     </BrowserRouter>
   );
