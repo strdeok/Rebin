8721350b80d15cd20b0f24dc584fd361c785ec03
strdeok <ejraks1548@gmail.com>
2025-06-15 20:45:47 +0900

    fix: 팝업으로 복구

--- src/pages/beforeLogin/login/login.tsx
+++ src/pages/beforeLogin/login/login.tsx
@@ -1,41 +1,20 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import {
-  getRedirectResult,
-  getAuth,
-  type UserCredential,
-  getAdditionalUserInfo,
-} from "firebase/auth";
-import signIn from "../../../utils/firebase/googleLogin";
+import siginIn from "../../../utils/firebase/googleLogin";
+import { getAdditionalUserInfo, type UserCredential } from "firebase/auth";
 
 export default function Login() {
   const navigate = useNavigate();
-
   useEffect(() => {
-    const handleLogin = async () => {
-      const auth = getAuth();
-
-      try {
-        const result = await getRedirectResult(auth);
-
-        if (result) {
-          const userInfo = getAdditionalUserInfo(result as UserCredential);
-          if (userInfo?.isNewUser) {
-            navigate("/signup");
-          } else {
-            navigate("/main");
-          }
-        } else {
-          await signIn();
-        }
-      } catch (err) {
-        console.error("로그인 에러:", err);
-        alert("로그인에 실패했습니다. 다시 시도해주세요.");
+    siginIn()
+      .then((res) => {
+        const userInfo = getAdditionalUserInfo(res as UserCredential);
+        userInfo?.isNewUser ? navigate("/signup") : navigate("/main");
+      })
+      .catch(() => {
+        alert("로그인에 문제가 발생하였습니다. 다시 시도해주세요.");
         navigate("/");
-      }
-    };
-
-    handleLogin();
+      });
   }, []);
 
   return (
